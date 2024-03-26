5893ec14d52e20e309a5dd21d5e4da2c49035349
Ygor Guilherme <100237670+ygorfsguilherme@users.noreply.github.com>
2024-03-26 16:28:27 -0300

    craindo filtro de nomes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, ()=>{ 
+app.get('/listagem', async() => {
+    return [
+        'amanda',
+        'ygor'
+    ]
+})
+
+app.listen(3000, () => {
     console.log('servidor rodando na porta 3000')
 })
