10cf04446aa5dbb40ff006e1e1e07122079aa47f
Amandalfs <amandalfs57@gmail.com>
2024-03-26 16:16:59 -0300

    feat: primeira lista

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.get('/lista', async () => {
+    return [
+        'amanda',
+        'ygor',
+        'patrick'
+    ]
+})
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
