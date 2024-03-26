f0450cff6859cc2a7c36eb4b2587a3104a7f6277
Amandalfs <amandalfs57@gmail.com>
2024-03-26 16:38:34 -0300

    feat: conflito resolvido

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/lista', async () => {
-    return [
-        'amanda',
-        'ygor',
-        'patrick'
-    ]
-})
-
 app.get('/listagem', async() => {
     return [
         'amanda',
