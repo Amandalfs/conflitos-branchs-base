a88db8722413dd7662d48c1416bffab7b2b375f7
Amandalfs <amandalfs57@gmail.com>
2024-03-27 15:26:40 -0300

    feat: projeto base

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/lista', async () => {
-    return [
-        'amanda',
-        'ygor',
-        'patrick'
-    ]
-})
-
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
