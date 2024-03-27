750a41ab57d3109ad4e3206055cad8792b75ae61
Amandalfs <amandalfs57@gmail.com>
2024-03-27 15:37:46 -0300

    feat: conflito

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.get('/lista/:id', (req, res)=>{ 
+    const lista = [{
+        id: 1,
+        nome: 'amanda'
+    },
+    {
+        id: 3,
+        nome: 'ygor'
+    }]
+    return lista.map((usuario) => usuario.id === req.params.id)
+})
+
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
