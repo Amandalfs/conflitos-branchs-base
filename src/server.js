9d96d3f54fa75e78e5a4dfad70a27776801179a9
Amandalfs <amandalfs57@gmail.com>
2024-04-02 15:33:45 -0300

    codigo base

--- src/server.js
+++ src/server.js
@@ -1,18 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/lista/:id', (req, res)=>{ 
-    const lista = [{
-        id: 1,
-        nome: 'amanda'
-    },
-    {
-        id: 3,
-        nome: 'ygor'
-    }]
-    return lista.map((usuario) => usuario.id === req.params.id)
-})
-
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
